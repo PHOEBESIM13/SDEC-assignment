721051e6016ed1a372295b15a2a518e4d7948c3e
PHOEBESIM13 <105427357+PHOEBESIM13@users.noreply.github.com>
2022-05-17 19:19:28 +0800

    Update final-coursework.py

--- final-coursework.py
+++ final-coursework.py
@@ -7,10 +7,10 @@ st.write("""
 This app predicts the **Iris flower** type!
 """)
 
-st.sidebar.header('User Input Parameters')
+st.sidebar.header('Select Parameters')
 
 def user_input_features():
-    sepal_length = st.sidebar.slider('Sepal length', 4.3, 7.9, 5.4)
+    sepal_length = st.multiselect('Sepal length', 4.3, 7.9, 5.4)
     sepal_width = st.sidebar.slider('Sepal width', 2.0, 4.4, 3.4)
     petal_length = st.sidebar.slider('Petal length', 1.0, 6.9, 1.3)
     petal_width = st.sidebar.slider('Petal width', 0.1, 2.5, 0.2)
@@ -29,6 +29,8 @@ st.write(df)
 iris = pd.read_csv('https://raw.githubusercontent.com/PHOEBESIM13/SDEC-assignment/main/IRIS.csv')
 X = iris.drop('species', axis=1)
 Y = iris.species
+
+
 
 clf = RandomForestClassifier()
 clf.fit(X, Y)
