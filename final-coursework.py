e4d90d4a916821137d70131f5e71f5dbda6e5f2d
PHOEBESIM13 <105427357+PHOEBESIM13@users.noreply.github.com>
2022-05-17 19:37:04 +0800

    Update final-coursework.py

--- final-coursework.py
+++ final-coursework.py
@@ -7,7 +7,7 @@ st.write("""
 This app predicts the **Iris flower** type!
 """)
 
-st.image("https://freerangestock.com/photos/94178/purple-iris-flower.html")
+st.image("https://images.unsplash.com/photo-1560778717-5f712067016c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80")
          
 st.sidebar.header('Select Parameters')
 
